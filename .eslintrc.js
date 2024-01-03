2d4f8e15d85d44a13d8ea6c013399f708e7535df
Viet Hoang <marcodev.haui@gmail.com>
2024-01-03 11:31:31 +0700

    feat: Docker compose

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'nestjs', 'import'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:nestjs/recommended'],
   root: true,
   env: {
     node: true,
@@ -21,5 +17,24 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'object-curly-spacing': ['error', 'always'],
+    semi: ['error', 'always'],
+    '@typescript-eslint/no-unused-vars': ['error', { vars: 'all' }],
+    'nestjs/use-validation-pipe': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    quotes: [2, 'single', { avoidEscape: true }],
   },
 };
